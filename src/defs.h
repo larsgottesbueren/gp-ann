d90e020e9aae1e5cf833bcdbab57ac78f28d883a
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2023-10-10 15:20:46 +0200

    Parallelize IO

--- src/defs.h
+++ src/defs.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <cstdint>
+#include <chrono>
 
 struct PointSet {
   std::vector<float> coordinates;   // potentially empty
@@ -16,4 +17,43 @@ using NNVec = std::vector<std::pair<float, uint32_t>>;
 struct HNSWParameters {
     int M = 16;
     int ef_construction = 200;
+};
+
+using Duration = std::chrono::duration<double>;
+using Timepoint = decltype(std::chrono::high_resolution_clock::now());
+
+struct Timer {
+    static Timer& GlobalTimer() {
+        static Timer GLOBAL_TIMER;
+        return GLOBAL_TIMER;
+    }
+    bool running = false;
+    Timepoint start;
+    Duration total_duration = Duration(0.0);
+
+    Timepoint Start() {
+        if (running) throw std::runtime_error("Called Start() but timer is already running");
+        start = std::chrono::high_resolution_clock::now();
+        running = true;
+        return start;
+    }
+
+    Duration Stop() {
+        if (!running) throw std::runtime_error("Timer not running but called Stop()");
+        auto finish = std::chrono::high_resolution_clock::now();
+        Duration duration = finish - start;
+        total_duration += duration;
+        running = false;
+        return duration;
+    }
+
+    Duration Restart() {
+        if (!running) throw std::runtime_error("Timer not running but called Restart()");
+        auto finish = std::chrono::high_resolution_clock::now();
+        Duration duration = finish - start;
+        total_duration += duration;
+        start = finish;
+        return duration;
+    }
+
 };
--- src/points_io.h
+++ src/points_io.h
@@ -5,22 +5,55 @@
 
 #include "defs.h"
 
+#include <parlay/parallel.h>
+
+size_t ComputeChunkSize(size_t a, size_t b) {
+    return (a+b-1) / b;
+}
+
 PointSet ReadPoints(const std::string& path, int64_t size = -1) {
-    std::ifstream in(path, std::ios::binary);
     uint32_t n, d;
-    if (size == -1) {
-        in.read(reinterpret_cast<char*>(&n), sizeof(uint32_t));
-    } else {
-        n = size;
+    size_t offset = 0;
+    {
+        std::ifstream in(path, std::ios::binary);
+        if (size == -1) {
+            in.read(reinterpret_cast<char*>(&n), sizeof(uint32_t));
+            offset += sizeof(uint32_t);
+        } else {
+            offset += sizeof(uint32_t);     // TODO this is for testing currently
+            n = size;
+        }
+        in.read(reinterpret_cast<char*>(&d), sizeof(uint32_t));
+        offset += sizeof(uint32_t);
     }
-    in.read(reinterpret_cast<char*>(&d), sizeof(uint32_t));
 
     std::cout << n << " " << d << std::endl;
 
     PointSet points;
     points.n = n; points.d = d;
+
+    Timer timer; timer.Start();
+
     points.coordinates.resize(points.n * points.d);
-    in.read(reinterpret_cast<char*>(&points.coordinates[0]), points.n * points.d * sizeof(float));
+
+    std::cout << "alloc + touch done. Took " << timer.Restart() << std::endl;
+
+    size_t num_chunks = 256;
+    size_t chunk_size = ComputeChunkSize(points.coordinates.size(), num_chunks);
+    parlay::parallel_for(0, num_chunks, [&](size_t i) {
+        std::ifstream in(path, std::ios::binary);
+        size_t begin = i * chunk_size;
+        size_t end = std::min(points.coordinates.size(), (i+1) * chunk_size);
+
+        in.seekg(offset + begin * sizeof(float));
+
+        in.read(reinterpret_cast<char*>(&points.coordinates[begin]), (end-begin) * sizeof(float));
+    }, 1);
+
+
+    std::cout << "Read took " << timer.Stop() << std::endl;
+
+    std::exit(0);
     return points;
 }
 
