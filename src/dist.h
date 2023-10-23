c015d4742e343f03abce2dabb82017ffcb024fd9
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2023-10-23 20:06:24 +0200

    implement correct version of efanna's SIMD L2Sqr

--- src/dist.h
+++ src/dist.h
@@ -85,6 +85,40 @@ namespace efanna2e {
             return result;
         }
 
+
+        float compare3(float* a, float* b, unsigned size) {
+            #define AVX_L2SQR(addr1, addr2, dest, tmp1, tmp2) \
+                              tmp1 = _mm256_loadu_ps(addr1);                  \
+                              tmp2 = _mm256_loadu_ps(addr2);                  \
+                              tmp1 = _mm256_sub_ps(tmp1, tmp2);               \
+                              tmp1 = _mm256_mul_ps(tmp1, tmp1);               \
+                              dest = _mm256_add_ps(dest, tmp1);
+
+
+            __m256 sum;
+            __m256 l0, l1;
+            __m256 r0, r1;
+            size_t qty16 = size >> 4;
+            size_t aligned_size = qty16 << 4;
+            const float *l = a;
+            const float *r = b;
+
+            float unpack[8] __attribute__((aligned(32))) = {0, 0, 0, 0, 0, 0, 0, 0};
+            sum = _mm256_loadu_ps(unpack);
+
+            for (unsigned i = 0; i < aligned_size; i += 16, l += 16, r += 16) {
+                AVX_L2SQR(l, r, sum, l0, r0);
+                AVX_L2SQR(l + 8, r + 8, sum, l1, r1);
+            }
+            _mm256_storeu_ps(unpack, sum);
+            float result = unpack[0] + unpack[1] + unpack[2] + unpack[3] + unpack[4] + unpack[5] + unpack[6] + unpack[7];
+            for (unsigned i = aligned_size; i < size; ++i, ++l, ++r) {
+                float diff = *l - *r;
+                result += diff * diff;
+            }
+            return result;
+        }
+
     };
 
 }  // namespace efanna2e
