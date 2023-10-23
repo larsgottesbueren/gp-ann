a81b0a731f86af01b7a5402ea56ab151506fe731
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2023-10-23 17:06:51 +0200

    remove bogus completely broken AVX L2 implementation from efanna & Co.

--- convert.cpp
+++ convert.cpp
@@ -12,7 +12,15 @@ int main(int argc, const char* argv[]) {
     }
     std::vector<float> Q = P;
     for (float& q : Q) q = q - 2.3f;
+
+    int num_bogus_entries = 214;
+    for (int i = 0; i < num_bogus_entries; ++i) {
+        P.push_back(-555);
+        Q.push_back(555);
+    }
+
     float dist = distance(&P[0], &Q[0], dim);
+
 
     std::cout << "Dist = " << dist << std::endl;
     std::cout << "expected = " << dim * (2.3f * 2.3f) << std::endl;
--- src/dist.h
+++ src/dist.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../external/hnswlib/hnswlib/space_l2.h"
+
 #include <x86intrin.h>
 #include <iostream>
 #include <algorithm>
@@ -20,88 +22,19 @@ namespace efanna2e {
         float compare(const float *a, const float *b, unsigned size) const {
             float result = 0;
 
-#ifdef __GNUC__
 #ifdef __AVX__
-
-#define AVX_L2SQR(addr1, addr2, dest, tmp1, tmp2) \
-      tmp1 = _mm256_loadu_ps(addr1);\
-      tmp2 = _mm256_loadu_ps(addr2);\
-      tmp1 = _mm256_sub_ps(tmp1, tmp2); \
-      tmp1 = _mm256_mul_ps(tmp1, tmp1); \
-      dest = _mm256_add_ps(dest, tmp1);
-
-            __m256 sum;
-            __m256 l0, l1;
-            __m256 r0, r1;
-            unsigned D = (size + 7) & ~7U;
-            unsigned DR = D % 16;
-            unsigned DD = D - DR;
-            std::cout << "size = " << size << " D = " << D << " DR = " << DR << " DD = " << DD << std::endl;
-            const float *l = a;
-            const float *r = b;
-            const float *e_l = l + DD;
-            const float *e_r = r + DD;
-            float unpack[8] __attribute__ ((aligned (32))) = {0, 0, 0, 0, 0, 0, 0, 0};
-
-            sum = _mm256_loadu_ps(unpack);
-            if(DR){AVX_L2SQR(e_l, e_r, sum, l0, r0);}
-
-            for (unsigned i = 0; i < DD; i += 16, l += 16, r += 16) {
-                AVX_L2SQR(l, r, sum, l0, r0);
-                AVX_L2SQR(l + 8, r + 8, sum, l1, r1);
-            }
-            _mm256_storeu_ps(unpack, sum);
-            result = unpack[0] + unpack[1] + unpack[2] + unpack[3] + unpack[4] + unpack[5] + unpack[6] + unpack[7];
-
+            size_t qty = size;
+            size_t qty16 = qty >> 4 << 4;
+            result = hnswlib::L2SqrSIMD16ExtAVX(a, b, &qty16);
+            size_t qty_left = qty - qty16;
+            result += hnswlib::L2Sqr(a + qty_left, b + qty_left, &qty_left);
 #else
-#ifdef __SSE2__
-#define SSE_L2SQR(addr1, addr2, dest, tmp1, tmp2) \
-          tmp1 = _mm_load_ps(addr1);\
-          tmp2 = _mm_load_ps(addr2);\
-          tmp1 = _mm_sub_ps(tmp1, tmp2); \
-          tmp1 = _mm_mul_ps(tmp1, tmp1); \
-          dest = _mm_add_ps(dest, tmp1);
-
-            __m128 sum;
-            __m128 l0, l1, l2, l3;
-            __m128 r0, r1, r2, r3;
-            unsigned D = (size + 3) & ~3U;
-            unsigned DR = D % 16;
-            unsigned DD = D - DR;
-            const float *l = a;
-            const float *r = b;
-            const float *e_l = l + DD;
-            const float *e_r = r + DD;
-            float unpack[4] __attribute__ ((aligned (16))) = { 0, 0, 0, 0 };
-
-            sum = _mm_load_ps(unpack);
-            switch (DR) {
-                case 12:
-                SSE_L2SQR(e_l + 8, e_r + 8, sum, l2, r2);
-                case 8:
-                SSE_L2SQR(e_l + 4, e_r + 4, sum, l1, r1);
-                case 4:
-                SSE_L2SQR(e_l, e_r, sum, l0, r0);
-                default:
-                    break;
-            }
-            for (unsigned i = 0; i < DD; i += 16, l += 16, r += 16) {
-                SSE_L2SQR(l, r, sum, l0, r0);
-                SSE_L2SQR(l + 4, r + 4, sum, l1, r1);
-                SSE_L2SQR(l + 8, r + 8, sum, l2, r2);
-                SSE_L2SQR(l + 12, r + 12, sum, l3, r3);
-            }
-            _mm_storeu_ps(unpack, sum);
-            result += unpack[0] + unpack[1] + unpack[2] + unpack[3];
-
-            //normal distance
-#else
-
             float diff0, diff1, diff2, diff3;
             const float* last = a + size;
             const float* unroll_group = last - 3;
 
             /* Process 4 items with each loop for efficiency. */
+            size_t dim = 0;
             while (a < unroll_group) {
                 diff0 = a[0] - b[0];
                 diff1 = a[1] - b[1];
@@ -110,16 +43,45 @@ namespace efanna2e {
                 result += diff0 * diff0 + diff1 * diff1 + diff2 * diff2 + diff3 * diff3;
                 a += 4;
                 b += 4;
+                dim += 4;
+            }
+            std::cout << "dim after " << dim;
+            /* Process last 0-3 pixels.  Not needed for standard vector lengths. */
+            while (a < last) {
+                diff0 = *a++ - *b++;
+                result += diff0 * diff0;
+                dim++;
+            }
+            std::cout << "dim final " << dim;
+#endif
+
+            return result;
+        }
+
+        float compare2(float* a, float* b, unsigned size) {
+            float result = 0.0f;
+            float diff0, diff1, diff2, diff3;
+            const float* last = a + size;
+            const float* unroll_group = last - 3;
+
+            /* Process 4 items with each loop for efficiency. */
+            size_t dim = 0;
+            while (a < unroll_group) {
+                diff0 = a[0] - b[0];
+                diff1 = a[1] - b[1];
+                diff2 = a[2] - b[2];
+                diff3 = a[3] - b[3];
+                result += diff0 * diff0 + diff1 * diff1 + diff2 * diff2 + diff3 * diff3;
+                a += 4;
+                b += 4;
+                dim += 4;
             }
             /* Process last 0-3 pixels.  Not needed for standard vector lengths. */
             while (a < last) {
                 diff0 = *a++ - *b++;
                 result += diff0 * diff0;
+                dim++;
             }
-#endif
-#endif
-#endif
-
             return result;
         }
 
