209b663c37c7bc5d63eb6aed5d3b2b144efe4c4e
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2023-10-24 13:44:03 +0200

    remove more debugging code

--- convert.cpp
+++ convert.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include "defs.h"
-#include "dist.h"
 #include "points_io.h"
 
 
 int main(int argc, const char* argv[]) {
-    std::string gtf = argv[1];
-    auto ground_truth = ReadGroundTruth(gtf);
-
-    #if false
     if (argc != 4) {
         std::cerr << "Usage ./Convert input-points output-points size" << std::endl;
         std::abort();
@@ -21,5 +16,4 @@ int main(int argc, const char* argv[]) {
 
     PointSet points = ReadPoints(input_file, n);
     WritePoints(points, output_file);
-    #endif
 }
