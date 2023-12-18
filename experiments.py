259b26fd0985317a2d4bcd3a57c15aea81fd6485
Lars Gottesbürren <gottesbuerren@i10pc110.iti.kit.edu>
2023-12-18 17:10:03 +0100

    changes to experiments.py script

--- experiments.py
+++ experiments.py
@@ -4,18 +4,28 @@ import subprocess
 data_path = 'data'
 
 datasets = [
-    # ('turing', 'L2'),
+    ('turing', 'L2'),
     ('deep', 'L2'),
     ('text-to-image', 'mips')
 ]
 
 partitioning_methods = [
-    'GP', 'KMeans',
-    'Pyramid',
+    #'GP', 
+    #'KMeans',
+    #'BalancedKMeans',
+    #'OGP',
+    'OGPS',
+    #'OBKM',
+    #'OKM',
+    #'Pyramid',
     # 'OurPyramid'
 ]
 
 num_shards_vals = [40]  # , 20, 10]
+
+overlap_values = [0.0, 0.2]
+
+overlapping_algos = ['OGP', 'OGPS', 'OBKM', 'OKM']
 
 num_neighbors = 10
 
@@ -23,7 +33,6 @@ build_folders = {
     'L2': 'release_l2',
     'mips': 'release_mips'
 }
-
 
 def create_builds():
     for dist, directory in build_folders.items():
@@ -48,8 +57,16 @@ def compute_partition(dataset, metric, part_method, num_shards):
                os.path.join(data_path, dataset + '_base1B.fbin'),
                os.path.join(data_path, dataset + '.partition'),
                str(num_shards), part_method]
-    print(arglist)
-    subprocess.call(arglist)
+    if part_method in overlapping_algos:
+        import copy
+        for o in overlap_values:
+            my_arglist = copy.copy(arglist)
+            my_arglist.append(str(o))
+            print(my_arglist)
+            subprocess.call(my_arglist)
+    else:
+        print(arglist)
+        subprocess.call(arglist)
 
 
 def compute_all_partitions():
@@ -59,14 +76,16 @@ def compute_all_partitions():
                 compute_partition(dataset, metric, part_method, num_shards)
 
 
-def run_query_set(dataset, metric, part_method, num_shards):
+def run_query_set(dataset, metric, part_method, num_shards, overlap):
     pfx = os.path.join(data_path, dataset)
     sfx = ''
+    if part_method in overlapping_algos:
+        sfx = '.o=' + str(overlap)
     arglist = [build_folders[metric] + '/QueryAttribution',
                pfx + '_base1B.fbin', pfx + '_query.fbin', pfx + '_ground-truth.bin',
                str(num_neighbors),
                pfx + '.partition.k=' + str(num_shards) + '.' + part_method + sfx,
-               "exp_outputs/" + dataset + "." + part_method + ".k=" + str(num_shards),
+               "exp_outputs/" + dataset + "." + part_method + ".k=" + str(num_shards) + sfx,
                part_method,
                str(num_shards)
                ]
@@ -78,7 +97,11 @@ def run_queries_on_all_datasets():
     for dataset, metric in datasets:
         for part_method in partitioning_methods:
             for num_shards in num_shards_vals:
-                run_query_set(dataset, metric, part_method, num_shards)
+                if part_method in overlapping_algos:
+                    run_query_set(dataset, metric, part_method, num_shards, 0.0)
+                else:
+                    for overlap in overlap_values:
+                        run_query_set(dataset, metric, part_method, num_shards, overlap)
 
 
 def pareto_filter(dataset, part_method, num_shards):
@@ -99,5 +122,5 @@ def run_all_pareto_filters():
                 pareto_filter(dataset, part_method, num_shards)
 
 
-# compute_all_partitions()
-run_queries_on_all_datasets()
+compute_all_partitions()
+#run_queries_on_all_datasets()
