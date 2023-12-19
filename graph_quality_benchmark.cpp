5b6ee3f32720954f29e54ff3a3984438cef5a80d
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2023-12-19 10:31:26 +0100

    IO for graph quality bench

--- graph_quality_benchmark.cpp
+++ graph_quality_benchmark.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <filesystem>
 
 #include "points_io.h"
+#include "recall.h"
+
 
 int main(int argc, const char* argv[]) {
+    std::string point_file = argv[1];
+    std::string query_file = argv[2];
+    std::string ground_truth_file = argv[3];
+    std::string k_string = argv[4];
+    int num_neighbors = std::stoi(k_string);
+
+    PointSet points = ReadPoints(point_file);
+    PointSet queries = ReadPoints(query_file);
+
+    std::vector<NNVec> ground_truth;
+    if (std::filesystem::exists(ground_truth_file)) {
+        ground_truth = ReadGroundTruth(ground_truth_file);
+        std::cout << "Read ground truth file" << std::endl;
+    } else {
+        std::cout << "start computing ground truth" << std::endl;
+        ground_truth = ComputeGroundTruth(points, queries, num_neighbors);
+        std::cout << "computed ground truth" << std::endl;
+        WriteGroundTruth(ground_truth_file, ground_truth);
+    }
+
 
 }
